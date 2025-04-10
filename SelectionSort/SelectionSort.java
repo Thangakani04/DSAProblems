6bb8d0768670e2cbb075fe21731e357d9d50e806
Thangakani04 <thangakani1113@gmail.com>
2025-04-10 22:47:35 +0530

    Selection sort has been implemented

---
+++ SelectionSort/SelectionSort.java
@@ -0,0 +1,44 @@
+package SelectionSort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+	
+	public static void main(String[] args) {
+		
+		int[] arr = {3,4,2,5,1};
+		selectionsort(arr);
+		System.out.println(Arrays.toString(arr));
+		
+	}
+
+	public static void selectionsort(int[] arr) {
+		
+		for(int i=0;i<arr.length;i++) {
+			int last = arr.length-i-1;
+			int maxindex = getMaxIndex(arr,0,last);
+			swap(arr,maxindex,last);
+		}
+		
+	}
+
+	public static void swap(int[] arr, int maxindex, int last) {
+		int temp = arr[maxindex];
+		arr[maxindex] = arr[last];
+		arr[last] = temp;
+		
+		
+	}
+
+	public static int getMaxIndex(int[] arr, int start, int end) {
+		int max =start;
+		
+		for(int i=start;i<=end;i++) {
+			if(arr[i] > arr[max]) {
+				max=i;
+			}
+		}
+		return max;
+	}
+
+}
