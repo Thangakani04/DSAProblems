92734542ebdf6747698a89db08fec33a133340cb
Thangakani04 <thangakani1113@gmail.com>
2025-08-07 13:34:17 +0530

    Recursion example

---
+++ Recursion/NumbersExampleRecursion.java
@@ -0,0 +1,26 @@
+package Recursion;
+
+public class NumbersExampleRecursion {
+	
+	public static void main(String[] args) {
+		
+		print(1);
+		
+	}
+
+	static void print(int n) {
+		
+		
+		if (n == 5) {
+			System.out.println(5);
+			return;
+		}
+		 
+		
+		System.out.println(n);
+		//recursive call
+		print(n+1);
+	}
+	
+
+}
