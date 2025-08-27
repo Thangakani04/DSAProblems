8ad36ed6672b9e12ab402e97c800c5a29fecf839
Thangakani04 <thangakani1113@gmail.com>
2025-08-27 13:03:09 +0530

    Array problems using recursion

--- Recursion/ArrayProblems/LinearSearch.java
+++ Recursion/ArrayProblems/LinearSearch.java
@@ -5,16 +5,16 @@ public class LinearSearch {
 	public static void main(String[] args) {
 		int[] arr= {3,2,1,18,9,90,32,6};
 		int target =4;
-		int ans = search(arr,0,target);
+		int ans = search(arr,target,0);
 		System.out.println(ans);
 		
 	}
 	
-	static int search(int[] arr, int index , int target) {
+	static int search(int[] arr, int target,int index) {
 		if(index >arr.length-1) {
 			return -1;
 		}
-		return arr[index] == target ? index :  search(arr,index+1,target);
+		return arr[index] == target ? index :  search(arr,target,index+1);
 	}
 
 }
