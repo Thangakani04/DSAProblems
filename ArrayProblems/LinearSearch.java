0a95af43bede59fabd1b24b97ecd245158a3ea6e
Thangakani04 <thangakani1113@gmail.com>
2025-08-27 12:56:44 +0530

    Linear search using recursion

---
+++ ArrayProblems/LinearSearch.java
@@ -0,0 +1,20 @@
+package ArrayProblems;
+
+public class LinearSearch {
+	
+	public static void main(String[] args) {
+		int[] arr= {3,2,1,18,9,90,32,6};
+		int target =90;
+		int ans = search(arr,0,target);
+		System.out.println(ans);
+		
+	}
+	
+	static int search(int[] arr, int index , int target) {
+		if(index ==arr.length-1) {
+			return index;
+		}
+		return arr[index] == target ? index :  search(arr,index+1,target);
+	}
+
+}
