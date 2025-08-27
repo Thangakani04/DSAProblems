fc13910bee8788b5d2ad3ca769647e28b61de433
Thangakani04 <thangakani1113@gmail.com>
2025-08-27 12:38:25 +0530

    Array problems using recursion , Sorted array

---
+++ ArrayProblems/SortedArray.java
@@ -0,0 +1,21 @@
+package ArrayProblems;
+
+public class SortedArray {
+	
+	public static void main(String[] args) {
+		//find if array is sorted or not
+		int[] arr = {1,2,3,8,5,6,7,9};
+		
+		System.out.println(sorted(arr,0));  //index starts from 0
+	}
+	
+	static boolean sorted(int[] arr, int index) {
+		//base condition
+		
+		if(index == arr.length-1) {
+			return true;
+		}
+		return arr[index] <arr[index+1] && sorted(arr,index+1);
+	}
+
+}
