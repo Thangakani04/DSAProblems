f1dc03f4a3887e6e0c54ce97e10affef4d6efae9
Thangakani04 <thangakani1113@gmail.com>
2025-08-29 21:43:16 +0530

    Linear search problem solved using recursion on multiple occurrences

--- Recursion/ArrayProblems/LinearSearch.java
+++ Recursion/ArrayProblems/LinearSearch.java
@@ -1,16 +1,21 @@
 package ArrayProblems;
 
+import java.util.ArrayList;
+
 public class LinearSearch {
 	
 	public static void main(String[] args) {
 		int[] arr= {3,2,1,18,9,9,32,6};
-		int target =91;
+		int target =9;
 		
 		System.out.println(findindex(arr,target,0));		
 		System.out.println(find(arr,target,0));	
 		System.out.println(findindex1(arr,target,0));
 		System.out.println(findback(arr,target,arr.length-1));
 		System.out.println(findindexback(arr,target,arr.length-1));
+		findAllIndex(arr,target,0);
+		System.out.println(list);
+		System.out.println(findAllIndexreturn(arr,target,0,new ArrayList<Integer>()));
 		
 	}
 	//my solution
@@ -49,6 +54,7 @@ public class LinearSearch {
 		}
 		return arr[index] ==target || findback(arr,target,index-1);
 	}
+	
 	static int findindexback(int[] arr, int target, int index) {
 		if(index == -1) {
 			return -1;
@@ -61,6 +67,26 @@ public class LinearSearch {
 		}
 	}
 	
+	static ArrayList<Integer> list = new ArrayList<>(); //if you keep this inside , when function repeats new object will be created every function call.
+	 static void findAllIndex(int[] arr,int target,int index) {
+		if(index==arr.length) {
+			return;
+		}
+		if(arr[index] == target) {
+			list.add(index);
+		}
+		findAllIndex(arr,target,index+1);
+	}
+	 
+	 static ArrayList<Integer> findAllIndexreturn(int[] arr,int target,int index,ArrayList<Integer> list) {
+			if(index==arr.length) {
+				return list;
+			}
+			if(arr[index] == target) {
+				list.add(index);
+			}
+			 return findAllIndexreturn(arr,target,index+1,list);
+		} 
 	
-
+	
 }
