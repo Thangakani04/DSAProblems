08763779ec645b3df572fa5b4d9cf9a9a42abb8d
Thangakani04 <thangakani1113@gmail.com>
2025-10-05 19:57:43 +0530

    Triangle pattern recursion solved

--- Recursion/PatternProblems/TrianglePatternRecursion.java
+++ Recursion/PatternProblems/TrianglePatternRecursion.java
@@ -4,10 +4,28 @@ public class TrianglePatternRecursion {
 	
 	public static void main(String[] args) {
 		
-		triangle(4,0);
+		//triangle(4,0);
+		//Normaltriangle(1,0);
+		triangle2(4,0);
 	}
 	
-	public static void triangle(int row, int col) {
+	 static void Normaltriangle(int row, int col) {
+		
+		 if(row>4) {
+			 return ;
+		 }
+		
+		if(row>col) {
+			System.out.print("*");	
+			Normaltriangle(row,col+1);
+		}
+		else {
+			System.out.println();  //next line
+			Normaltriangle(row+1,0);
+		}
+	}
+	
+	 static void triangle(int row, int col) {
 		if(row ==0) {
 			return;
 		}
@@ -21,5 +39,22 @@ public class TrianglePatternRecursion {
 			triangle(row-1,0);
 		}
 	}
+	 
+	 static void triangle2(int row, int col) {
+			if(row ==0) {
+				return;
+			}
+			
+			if(row>col) {
+				triangle2(row,col+1);
+				System.out.print("*");
+			}
+			else {
+				
+				triangle2(row-1,0);
+				System.out.println();  //next line
+				
+			}
+		}
 
 }
