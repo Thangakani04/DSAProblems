f691d73557873f4e18f4c697a384a7e78255cf17
Thangakani04 <thangakani1113@gmail.com>
2025-11-12 16:00:25 +0530

    Subset problem solved using recursion

--- Recursion/Subsets Problems/SubsetCreation.java
+++ Recursion/Subsets Problems/SubsetCreation.java
@@ -1,12 +1,15 @@
 package Recursion.Subsets;
 
+import java.util.ArrayList;
+
 public class SubsetCreation {
 	
 	public static void main(String[] args) {
 		
 		//subset is for arrays
 		//subseq is for strings
-		subseq("","abc");
+		//subseq("","abc");
+		System.out.println(subseqReturn("","abc"));
 	}
 	
 	static void subseq(String p, String up) {
@@ -19,7 +22,19 @@ public class SubsetCreation {
 		subseq(p+ch , up.substring(1));  //add it
 		subseq(p  , up.substring(1));  //remove it
 		
-		
+	}
+	
+	static ArrayList<String> subseqReturn(String p, String up){
+		if(up.isEmpty()) {
+			ArrayList<String> list = new ArrayList<>();
+			list.add(p);
+			return list;
+		}
+		char ch = up.charAt(0);
+		ArrayList<String> left = subseqReturn(p+ch, up.substring(1)); //add it
+		ArrayList<String> right = subseqReturn(p,up.substring(1)); //remove it
+		left.addAll(right);
+		return left;
 	}
 
 }
